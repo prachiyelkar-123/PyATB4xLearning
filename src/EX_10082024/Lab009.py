f8cd12671e05b2d2916bc194ade257b4f19c4530
prachi <prachiyelkar096@gmail.com>
2024-08-17 08:35:09 +0530

    Lab002 commit

--- src/EX_10082024/Lab009.py
+++ src/EX_10082024/Lab009.py
@@ -1,3 +1,9 @@
 import keyword
 
 print(keyword.kwlist)
+
+"""●	Keywords are also called Reserved Words.
+●	All the keywords can be in Lower Case or Upper Case.
+●	We cannot use a keyword as a variable name, function name or any other identifier. 
+●	They are used to define the syntax and structure of the Python language. In Python, keywords are case-sensitive.
+"""
